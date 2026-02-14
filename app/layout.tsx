b96d2a5ef7a10df6cc93a36616d2484377c33857
Vlad2346543 <114310954+Vlad2346543@users.noreply.github.com>
2026-02-14 12:24:33 +0200

    install commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,19 +2,23 @@ import Header from '../components/Header/Header';
 import Footer from '../components/Footer/Footer';
 import TanStackProvider from '../components/TanStackProvider/TanStackProvider';
 
-export default function RootLayout({
-  children,
-}: {
+interface Props {
   children: React.ReactNode;
-}) {
+  modal: React.ReactNode;
+}
+
+export default function RootLayout({ children, modal }: Props) {
   return (
     <html lang="en">
       <body>
         <TanStackProvider>
           <Header />
           {children}
+          {modal}
           <Footer />
         </TanStackProvider>
+
+        <div id="modal-root" />
       </body>
     </html>
   );
--- app/notes/filter/[...slug]/Notes.client.tsx
+++ app/notes/filter/[...slug]/Notes.client.tsx
@@ -1,5 +1,80 @@
 'use client';
 
-export default function Notes() {
-  return null;
+import { useState, useMemo } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import { fetchNotes } from '@/lib/api';
+
+import SearchBox from '@/components/SearchBox/SearchBox';
+import Pagination from '@/components/Pagination/Pagination';
+import Modal from '@/components/Modal/Modal';
+import NoteForm from '@/components/NoteForm/NoteForm';
+import NoteList from '@/components/NoteList/NoteList';
+
+interface Props {
+  tag: string;
+}
+
+export default function Notes({ tag }: Props) {
+  const [page, setPage] = useState(1);
+  const [search, setSearch] = useState('');
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const debouncedSearch = useDebounce(search, 500);
+
+  const { data } = useQuery({
+    queryKey: ['notes', page, tag, debouncedSearch],
+    queryFn: () =>
+      fetchNotes({
+        page,
+        perPage: 12,
+        search: debouncedSearch || undefined,
+        tag: tag === 'all' ? undefined : tag,
+      }),
+    placeholderData: prev => prev,
+  });
+
+  const handleSearch = (value: string) => {
+    setPage(1);
+    setSearch(value);
+  };
+
+  const toggleModal = () => setIsModalOpen(prev => !prev);
+
+  return (
+    <>
+      <SearchBox onSearch={handleSearch} />
+
+      <button onClick={toggleModal}>Create note</button>
+
+      {data && <NoteList notes={data.notes} />}
+
+      {data && (
+        <Pagination
+          page={page}
+          totalPages={data.totalPages}
+          onChange={setPage}
+        />
+
+      )}
+
+      {isModalOpen && (
+        <Modal onClose={toggleModal}>
+          <NoteForm onClose={toggleModal} />
+        </Modal>
+      )}
+    </>
+  );
+}
+
+
+
+function useDebounce<T>(value: T, delay: number): T {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useMemo(() => {
+    const id = setTimeout(() => setDebouncedValue(value), delay);
+    return () => clearTimeout(id);
+  }, [value, delay]);
+
+  return debouncedValue;
 }
